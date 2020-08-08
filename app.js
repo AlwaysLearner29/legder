89593df154072e8a218f80b512326bb45ae0ba35
Aryaman <alwayslearnerz29@gmail.com>
2020-08-08 15:46:38 +0530

    added files

---
+++ app.js
@@ -0,0 +1,36 @@
+// Initialize and add the map
+function initMap() {
+    // Your location
+    const loc = { lat: 42.361145, lng: -71.057083 };
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map'), {
+      zoom: 14,
+      center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+  }
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a,.btn").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
